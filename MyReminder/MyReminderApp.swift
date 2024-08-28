5e0d5472c591d0224a2cf59ad62aa12e5c147636
= <useraccont1507@gmail.com>
2024-08-28 12:01:24 +0300

    Initial Commit

---
+++ MyReminder/MyReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyReminderApp.swift
+//  MyReminder
+//
+//  Created by User on 28.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
