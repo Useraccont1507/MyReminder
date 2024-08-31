f31e215691d7de7ad52d57088f54295e315db3b7
= <useraccont1507@gmail.com>
2024-08-31 14:47:49 +0300

    added new screen editTask

---
+++ MyReminder/Views/EditTask.swift
@@ -0,0 +1,18 @@
+//
+//  EditTask.swift
+//  MyReminder
+//
+//  Created by User on 31.08.2024.
+//
+
+import SwiftUI
+
+struct EditTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTask()
+}
