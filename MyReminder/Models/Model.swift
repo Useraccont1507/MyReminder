7d13bfeaf5489fd2b00805d6512df1053cd5ff94
Illia Verezei <useraccont1507@gmail.com>
2025-03-08 17:45:40 +0200

    deleted unnecesary files

--- MyReminder/Models/Model.swift
+++ MyReminder/Models/Model.swift
@@ -9,38 +9,38 @@ import Foundation
 import SwiftUI
 
 struct Task: Identifiable, Equatable {
-  var id = UUID()
-  var notificationIdentifier = UUID().uuidString
-  var title: String
-  var body: String?
-  var priority: TaskPriority
-  var isCompleted: Bool
-  var date: Date
-  var dateTextView: Text {
-    let dateFormatter = DateFormatter()
-    dateFormatter.locale = .current
-    dateFormatter.timeZone = .current
-    dateFormatter.calendar = .current
-    dateFormatter.dateFormat = "MMM d, HH:mm"
-    
-    var color: Color = .gray
-    if date <= Date.now && !isCompleted {
-      color = .red
+    var id = UUID()
+    var notificationIdentifier = UUID().uuidString
+    var title: String
+    var body: String?
+    var priority: TaskPriority
+    var isCompleted: Bool
+    var date: Date
+    var dateTextView: Text {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .current
+        dateFormatter.timeZone = .current
+        dateFormatter.calendar = .current
+        dateFormatter.dateFormat = "MMM d, HH:mm"
+        
+        var color: Color = .gray
+        if date <= Date.now && !isCompleted {
+            color = .red
+        }
+        
+        if #available(iOS 17.0, *) {
+            return Text(dateFormatter.string(from: date)).foregroundStyle(color)
+        } else {
+            return Text(dateFormatter.string(from: date)).foregroundColor(color)
+        }
     }
-    
-    if #available(iOS 17.0, *) {
-      return Text(dateFormatter.string(from: date)).foregroundStyle(color)
-    } else {
-      return Text(dateFormatter.string(from: date)).foregroundColor(color)
-    }
-  }
 }
 
 enum TaskPriority: String, CaseIterable, Identifiable {
-  case important = "important"
-  case normal = "normal"
-  
-  var id: UUID {
-    UUID()
-  }
+    case important = "important"
+    case normal = "normal"
+    
+    var id: UUID {
+        UUID()
+    }
 }
---
+++ MyReminder/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MyReminder
+//
+//  Created by Illia Verezei on 07.03.2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello)")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
